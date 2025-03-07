8ea9231eb74bf65de992f0268d669db2764477f6
FedorManko <tfma94@gmail.com>
2025-03-07 08:38:42 +0300

    Added docker container configurations

--- src/main/java/org/manko/monitorsensors/controller/SensorController.java
+++ src/main/java/org/manko/monitorsensors/controller/SensorController.java
@@ -1,8 +1,9 @@
 package org.manko.monitorsensors.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * This is a class that describes entity rest controller for sensor.
@@ -10,7 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author f.manko
  * @since 06.03.2025
  */
-@Repository
+@RestController
 @RequestMapping("/api/v1/sensors")
 @RequiredArgsConstructor
-public class SensorController {}
+public class SensorController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+}
